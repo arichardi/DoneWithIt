a40ffe93afa613957186d0679ea3a1a2494b6fa9
Andre Richardi <a.richardi@gmail.com>
2021-07-18 14:40:46 -0300

    Listing details WIP

--- App.js
+++ App.js
@@ -2,14 +2,15 @@
 import React from 'react';
 import { View } from 'react-native';
 import AppButton from './app/components/AppButton';
+import ListingDetailScreen from './app/screens/ListingDetailScreen';
 import TestComponent from './app/screens/TestComponent';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <>
-  <TestComponent/>
+  <>
+    <ListingDetailScreen />
   </>
   );
 }
---
+++ app/screens/ListingDetailScreen.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import AppText from '../components/AppText'
+import colors from '../config/colors'
+
+const ListingDetailScreen = () => {
+    return (
+        <View>
+            <Image style={styles.image} source={require('../assets/jacket.jpg')} />
+            <View style={styles.detailsContainer}>
+                <AppText style={styles.title} >Red Jacket for sale</AppText>
+                <AppText style={styles.price} >100$</AppText>
+            </View>
+        </View>
+    )
+}
+
+export default ListingDetailScreen
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 300,
+    },
+    detailsContainer: {
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    price: {
+        color: colors.secondary,
+        fontSize: 20,
+        fontWeight: '700',
+        marginVertical: 10,
+    },
+})
